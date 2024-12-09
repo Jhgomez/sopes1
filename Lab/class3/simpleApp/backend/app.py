a1e38b0bae7595cb7e04c026290e37995ca70409
Juan Gomez <juan.e.g.f@outlook.com>
2024-12-09 11:34:29 -0600

    bugfix: backend

--- Lab/class3/simpleApp/backend/app.py
+++ Lab/class3/simpleApp/backend/app.py
@@ -70,5 +70,6 @@ def registro():
         jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, host='0.0.0.0', port=8000)
