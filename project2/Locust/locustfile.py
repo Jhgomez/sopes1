cdb14ff1225b00c00f0d7cdf5f48eb8757b9a156
Juan Gomez <juan.e.g.f@outlook.com>
2025-01-01 13:39:07 -0600

    chore: add code to download artifact using ORAS and delete it as needed

--- project2/Locust/locustfile.py
+++ project2/Locust/locustfile.py
@@ -1,4 +1,4 @@
-import time, json, random
+import time, json, random, os
 from locust import HttpUser, task
 
 class PostCoursesTest(HttpUser):
@@ -10,13 +10,17 @@ class PostCoursesTest(HttpUser):
       courses = json.load(file)
     
     for _ in range(len(courses)):
-      response = self.client.post("http://localhost:8000/course", json=courses.pop(), name="/coursePost")
+      assignation = courses.pop()
+      print(assignation)
+      response = self.client.post("http://localhost:8000/course", json=assignation, name="/coursePost")
       print(response.text)
       print(response.status_code)
       time.sleep(0.3)
 
   def on_start(self):
+    os.system('oras pull --insecure 35.223.33.184.nip.io/sopes1/courses:latest')
     print("task started")
 
   def on_stop(self):
+    os.system('del courses.json')
     print("task end")
