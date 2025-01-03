cca18ec8f81d37857ded6ab9d1bf90a804e5d9f6
Juan Gomez <juan.e.g.f@outlook.com>
2025-01-02 22:43:59 -0600

    chore: generator

--- project2/Locust/gradesJsonGenerator.py
+++ project2/Locust/gradesJsonGenerator.py
@@ -12,37 +12,49 @@ class GradesJsonGenerator:
     for x in range(int(entries)):
       gradeEntry = { 'curso': random.choice(cursos), 'facultad': "Ingenieria", 'carrera': random.choice(carrera), 'region': self._getRegion() }
       self.grades.append(gradeEntry)
+
+
+
     
   def _generateMedicina(self, fileName, entries):
-    cursos = ["MD1", "MG", "Inyecciones", "PA2", "PED"]
+# "Inyecciones", "PA2", "PED"
+    cursos = ["MD1", "MG"]
     carrera = ["General", "Pediatria", "Cirugia", "Oftalmologia"]
     
     for x in range(int(entries)):
       gradeEntry = { 'curso': random.choice(cursos), 'facultad': "Medicina", 'carrera': random.choice(carrera), 'region': self._getRegion() }
       self.grades.append(gradeEntry)
 
-  def _generateArquitectura(self, fileName, entries):
-    cursos = ["Autocad", "Logos", "Planos", "Medidas", "Estructuras"]
-    carrera = ["Diseño Grafico", "Interiores", "Exteriores", "Arquitecto"]
-    
-    for x in range(int(entries)):
-      gradeEntry = { 'curso': random.choice(cursos), 'facultad': "Arquitectura", 'carrera': random.choice(carrera), 'region': self._getRegion() }
-      self.grades.append(gradeEntry)
 
-  def _generateHumanidades(self, fileName, entries):
-    cursos = ["M1", "Filo2", "TS1", "H2", "Soci2"]
-    carrera = ["Pedadogia", "Arte", "Filosofia", "Trabajador Social"]
+
+
+  # def _generateArquitectura(self, fileName, entries):
+  #   cursos = ["Autocad", "Logos", "Planos", "Medidas", "Estructuras"]
+  #   carrera = ["Diseño Grafico", "Interiores", "Exteriores", "Arquitecto"]
     
-    for x in range(int(entries)):
-      gradeEntry = { 'curso': random.choice(cursos), 'facultad': "Humanidades", 'carrera': random.choice(carrera), 'region': self._getRegion() }
-      self.grades.append(gradeEntry)
+  #   for x in range(int(entries)):
+  #     gradeEntry = { 'curso': random.choice(cursos), 'facultad': "Arquitectura", 'carrera': random.choice(carrera), 'region': self._getRegion() }
+  #     self.grades.append(gradeEntry)
+
+      
+
+  # def _generateHumanidades(self, fileName, entries):
+  #   cursos = ["M1", "Filo2", "TS1", "H2", "Soci2"]
+  #   carrera = ["Pedadogia", "Arte", "Filosofia", "Trabajador Social"]
+    
+  #   for x in range(int(entries)):
+  #     gradeEntry = { 'curso': random.choice(cursos), 'facultad': "Humanidades", 'carrera': random.choice(carrera), 'region': self._getRegion() }
+  #     self.grades.append(gradeEntry)
+
+
+
 
   def __init__(self, fileName, entries):
     self.grades = []
     self._generateIngenieria(fileName, entries*0.4)
-    self._generateArquitectura(fileName, entries*0.3)
+    # self._generateArquitectura(fileName, entries*0.3)
     self._generateMedicina(fileName, entries*0.2)
-    self._generateHumanidades(fileName, entries*0.1)
+    # self._generateHumanidades(fileName, entries*0.1)
 
     with open(f"./{fileName}.json", 'w') as file:
       json.dump(self.grades, file, indent = 2, ensure_ascii=False)
