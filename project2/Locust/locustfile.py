029d7fe8a8f29750aa30f92650d36466888d0a8b
Juan Gomez <juan.e.g.f@outlook.com>
2025-01-06 12:03:50 -0600

    chore: stop calling oras

--- project2/Locust/locustfile.py
+++ project2/Locust/locustfile.py
@@ -12,16 +12,16 @@ class PostCoursesTest(HttpUser):
     for _ in range(len(courses)):
       assignation = courses.pop()
       print(assignation)
-      response = self.client.post("http://localhost:8000/course", json=assignation, name="/coursePost")
+      response = self.client.post("http://34.57.250.18.nip.io/course", json=assignation, name="/coursePost")
       print(response.text)
       print(response.status_code)
       time.sleep(0.3)
 
   def on_start(self):
     #os.system('oras pull --insecure 35.223.33.184.nip.io/sopes1/courses:latest')
-    os.system('oras pull --insecure core.harbor.sopes/sopes1/courses:latest')
+    #os.system('oras pull --insecure core.harbor.sopes/sopes1/courses:latest')
     print("task started")
 
   def on_stop(self):
-    os.system('del courses.json')
+    #os.system('del courses.json')
     print("task end")
